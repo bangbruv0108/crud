fc00e246a63c8ef028dfb3bbaa17c974197e41bb
Dhruv <dhruvofficial0108@gmail.com>
2024-07-28 14:00:22 +0530

    selected value will appear when you click on classname

--- edit.php
+++ edit.php
@@ -20,38 +20,56 @@
     }else{
         echo"query unsuccesfull";
     }
-    
+
 
    if(mysqli_num_rows($result)> 0){
 
-
-
+    while($row=mysqli_fetch_assoc($result)){
     ?>
     <form class="post-form" action="updatedata.php" method="post">
       <div class="form-group">
           <label>Name</label>
-          <input type="hidden" name="sid" value=""/>
-          <input type="text" name="sname" value=""/>
+          <input type="hidden" name="sid" value="<?php echo $row['sid']?>"/>
+          <input type="text" name="sname" value="<?php echo $row['sname']?>"/>
       </div>
       <div class="form-group">
           <label>Address</label>
-          <input type="text" name="saddress" value=""/>
+          <input type="text" name="saddress" value="<?php echo $row['address']?>"/>
       </div>
       <div class="form-group">
           <label>Class</label>
-          <select name="sclass">
-              <option value="" selected disabled>Select Class</option>
-              <option value="1">BCA</option>
-              <option value="2">BSC</option>
-              <option value="3">B.TECH</option>
-          </select>
+          <?php 
+          $sql1="SELECT * FROM studentclass";
+          $result1= mysqli_query($conn,$sql1) ;
+
+        
+      
+      
+         if(mysqli_num_rows($result1)> 0){
+            echo '  <select name="sclass">';
+      
+          while($row1=mysqli_fetch_assoc($result1)){
+          if($row['sclass']==$row1['cid'])
+          {
+            $select="selected";
+          }else{
+            $select="";
+          }
+            echo "<option {$select} gitvalue='{$row1['cid']}'>{$row1['cname']}</option>";
+          }
+         echo  "</select>";
+        }
+        ?>
+
       </div>
       <div class="form-group">
           <label>Phone</label>
-          <input type="text" name="sphone" value=""/>
+          <input type="text" name="sphone" value="<?php echo $row['sphone']?>"/>
       </div>
       <input class="submit" type="submit" value="Update"/>
     </form>
+    <?php }
+    } ?>
 </div>
 </div>
 </body>
